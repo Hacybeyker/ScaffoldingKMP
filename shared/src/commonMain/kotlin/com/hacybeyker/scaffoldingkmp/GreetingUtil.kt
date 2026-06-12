60f01ced26e006d00a16411f8a465ff8e95c9de2
Carlos Osorio <hacybeyker@gmail.com>
2026-06-12 17:43:23 -0500

    initialize Kotlin Multiplatform project with Android and iOS support

---
+++ shared/src/commonMain/kotlin/com/hacybeyker/scaffoldingkmp/GreetingUtil.kt
@@ -0,0 +1,4 @@
+package com.hacybeyker.scaffoldingkmp
+
+fun sayHello(to: String): String =
+    "Hello, $to!"
---
+++ shared/src/iosMain/kotlin/com/hacybeyker/scaffoldingkmp/MainViewController.kt
@@ -0,0 +1,5 @@
+package com.hacybeyker.scaffoldingkmp
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
